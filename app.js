feb15ea213554a02fd760073590fca0caf123b24
Joe <laulettaj@gmail.com>
2023-10-04 14:59:09 -0400

    change title to tubescreamer

--- app.js
+++ app.js
@@ -1,150 +1,140 @@
+// IMPORTS
+
+import Knob from "./knob";
+
 // DOM
 
-const power = document.querySelector('.power-led');
-const engage = document.querySelector('.engage-button');
-const odEl = document.querySelector('.overdrive');
-const toneEl = document.querySelector('.tone');
-const levelEl = document.querySelector('.level');
-const play = document.querySelector('.play');
-const ringModEl = document.querySelector('.ringmod');
-const modKnobEl = document.querySelector('.rm');
-const fileEl = document.querySelector('#file');
+const power = document.querySelector(".power-led");
+const engage = document.querySelector(".engage-button");
+const odEl = document.querySelector(".overdrive");
+const toneEl = document.querySelector(".tone");
+const levelEl = document.querySelector(".level");
+const play = document.querySelector(".play");
+const ringModEl = document.querySelector(".ringmod");
+const modKnobEl = document.querySelector(".rm");
+const fileEl = document.querySelector("#file");
 
-// VARIABLES
+// STATE
+
+const MAX_DIST = 3;
+const MAX_FREQ = 6000;
+const MAX_RMF = 55;
 
 let powerOn = false;
 let doubleTapTimer = null;
 let stopTimer = null;
-let currentFile = null;
-const MAX_DIST = 3;
-const MAX_FREQ = 6000; 
-const MAX_RMF = 55;
-
-// TONE.JS
+let storedVol = null;
+let storedFile = null;
 
 const context = new AudioContext();
-let storedVol = null;
-let timeLeft = null;
-
 let player = new Tone.Player("sample.mp3").toDestination();
 const dist = new Tone.Distortion(MAX_DIST / 2).toDestination();
-const filter = new Tone.Filter(MAX_FREQ / 2, 'peaking').toDestination();
+const filter = new Tone.Filter(MAX_FREQ / 2, "peaking").toDestination();
+const ringMod = new Tone.FrequencyShifter(MAX_RMF / 2).toDestination();
 filter.gain.value = 20;
 filter.Q.value = 1.5;
 filter.rolloff = -12;
-const ringMod = new Tone.FrequencyShifter(MAX_RMF / 2).toDestination();
-
 Tone.Master.volume.value = -10;
 player.sync().start(0);
 
-// MAKE KNOB CLASS
-
 class Knob {
-    constructor(element, initPos, initAngle) {
-        this.element = element;
-        this.initPos = initPos;
-        this.initAngle = initAngle;
-    }
+  constructor(element, initPos, initAngle) {
+    this.element = element;
+    this.initPos = initPos;
+    this.initAngle = initAngle;
+  }
 
-    handleReset = () => {    
-        this.element.style.transform = `rotate(0deg)`;
-        this.initAngle = 0;
-        if (this == level) {
-            if (powerOn) {
-                Tone.Master.volume.value = -14;
-            }
-        }
-        else if (this == tone) {
-            filter.frequency.value = MAX_FREQ / 2;
-        }
-        else if (this == od) {
-            dist.distortion = MAX_DIST / 2;
-        }
-        else if (this == modKnob) {
-            ringMod.frequency.value = MAX_RMF / 2;
-        }
+  handleReset = () => {
+    this.element.style.transform = `rotate(0deg)`;
+    this.initAngle = 0;
+    if (this == level) {
+      if (powerOn) {
+        Tone.Master.volume.value = -14;
+      }
+    } else if (this == tone) {
+      filter.frequency.value = MAX_FREQ / 2;
+    } else if (this == od) {
+      dist.distortion = MAX_DIST / 2;
+    } else if (this == modKnob) {
+      ringMod.frequency.value = MAX_RMF / 2;
     }
+  };
 
-    handleResetTouch = () => {
-        if (!doubleTapTimer) {
-            doubleTapTimer = setTimeout(() => {
-                clearTimeout(doubleTapTimer);
-                doubleTapTimer = null;
-            }, 200);
-            return;
-        }
-        // if knob is touched during the 200 ms window where timer has a value, reset
-        this.handleReset();
+  handleResetTouch = () => {
+    if (!doubleTapTimer) {
+      doubleTapTimer = setTimeout(() => {
+        clearTimeout(doubleTapTimer);
+        doubleTapTimer = null;
+      }, 200);
+      return;
     }
+    // if knob is touched during the 200 ms window where timer has a value, reset
+    this.handleReset();
+  };
 
-    updatePosition = (e) => {
-        let {element, initPos, initAngle} = this;
-        let currentPos;
-        
-        if (e.type == 'touchmove') {
-           const touch = e.touches[0];
-            currentPos = touch.clientY;
+  updatePosition = (e) => {
+    let { element, initPos, initAngle } = this;
+    let currentPos;
+
+    // ROTATION
+
+    if (e.type == "touchmove") {
+      const touch = e.touches[0];
+      currentPos = touch.clientY;
+    } else {
+      currentPos = e.clientY;
+    }
+    const delta = initPos - currentPos;
+    const rotation = delta * 1.5;
+    const newAngle = initAngle + rotation;
+
+    if (newAngle >= -150 && newAngle <= 150) {
+      element.style.transform = `rotate(${newAngle}deg)`;
+
+      // EFFECT
+
+      if (this == level) {
+        if (powerOn) {
+          Tone.Master.volume.value = ((newAngle + 150) / 300) * 32 - 30;
         } else {
-            currentPos = e.clientY;
+          storedVol = ((newAngle + 150) / 300) * 32 - 30;
         }
-        const delta = initPos - currentPos;
-        const rotation = delta*1.5;  
-        const newAngle = initAngle + rotation;
-        
-        if (newAngle >= -150 && newAngle <= 150) {
-            element.style.transform = `rotate(${newAngle}deg)`;
-
-            // EFFECT
-
-            if (this == level) {
-                if (powerOn) {
-                    Tone.Master.volume.value = 
-                    (((newAngle + 150) / 300) * 32) - 30;
-                } else {
-                    storedVol = (((newAngle + 150) / 300) * 32) - 30; 
-                }
-            }
-            else if (this == tone) {
-                filter.frequency.value = 
-                ((newAngle + 150) / 300) * MAX_FREQ;
-            }
-            else if (this == od) {
-                dist.distortion = 
-                ((newAngle + 150) / 300) * MAX_DIST;
-            }
-            else if (this == modKnob) {
-                ringMod.frequency.value = 
-                ((newAngle + 150) / 300) * MAX_RMF;
-            }
-        }
-    } 
-
-    handleKnob = (e) => {
-        if (e.type == 'touchstart') {
-            const touch = e.touches[0];
-            this.initPos = touch.clientY;
-        } else {
-            this.initPos = e.clientY;
-        }
-
-        window.addEventListener('mousemove', this.updatePosition);
-        window.addEventListener('touchmove', this.updatePosition);
-        window.addEventListener('mouseup', this.handleMouseUp);
-        window.addEventListener('touchend', this.handleMouseUp);
-    } 
-    
-    handleMouseUp = () => {
-        window.removeEventListener('mousemove', this.updatePosition);
-        window.removeEventListener('touchmove', this.updatePosition);
-        window.removeEventListener('mouseup', this.handleMouseUp);
-        window.removeEventListener('touchend', this.handleMouseUp);
-        
-        if (this.element.style.transform)
-        this.initAngle = parseInt(this.element.style.transform.split('(')[1].split('d')[0]);
+      } else if (this == tone) {
+        filter.frequency.value = ((newAngle + 150) / 300) * MAX_FREQ;
+      } else if (this == od) {
+        dist.distortion = ((newAngle + 150) / 300) * MAX_DIST;
+      } else if (this == modKnob) {
+        ringMod.frequency.value = ((newAngle + 150) / 300) * MAX_RMF;
+      }
     }
+  };
+
+  handleKnob = (e) => {
+    if (e.type == "touchstart") {
+      const touch = e.touches[0];
+      this.initPos = touch.clientY;
+    } else {
+      this.initPos = e.clientY;
+    }
+
+    window.addEventListener("mousemove", this.updatePosition);
+    window.addEventListener("touchmove", this.updatePosition);
+    window.addEventListener("mouseup", this.handleMouseUp);
+    window.addEventListener("touchend", this.handleMouseUp);
+  };
+
+  handleMouseUp = () => {
+    window.removeEventListener("mousemove", this.updatePosition);
+    window.removeEventListener("touchmove", this.updatePosition);
+    window.removeEventListener("mouseup", this.handleMouseUp);
+    window.removeEventListener("touchend", this.handleMouseUp);
+
+    if (this.element.style.transform)
+      this.initAngle = parseInt(
+        this.element.style.transform.split("(")[1].split("d")[0]
+      );
+  };
 }
-
-// INSTANTIATE KNOBS
 
 const od = new Knob(odEl, null, 0);
 const tone = new Knob(toneEl, null, 0);
@@ -152,127 +142,137 @@ const level = new Knob(levelEl, null, 0);
 const modKnob = new Knob(modKnobEl, null, 0);
 const knobs = [od, tone, level, modKnob];
 
-// INITIALIZE APP
-
-const handleEngage = () => {
-    if (powerOn) {
-        power.classList.remove('on');
-        powerOn = false;
-
-        dist.disconnect();
-        Tone.Master.volume.value = -10;
-
-        document.body.classList.remove('ring-mod-on');
-        ringModEl.classList.remove('ring-mod-on-button');
-        modKnobEl.classList.add('off');
-        return;
-    }
-    power.classList.add('on');
-    powerOn = true;
-
-    Tone.Master.volume.value = storedVol;
-    player.chain(dist, filter, ringMod);
-
-    if (ringModEl.ariaChecked == 'false') {
-        ringMod.wet.value = 0;
-    }
-    else {
-        document.body.classList.add('ring-mod-on');
-        ringModEl.classList.add('ring-mod-on-button');
-        modKnobEl.classList.remove('off');
-    }
-}
-
-const handleRingMod = () => {
-    if (powerOn) {
-        if (ringModEl.ariaChecked == 'false') {
-            ringMod.wet.value = 1;
-            ringModEl.ariaChecked = 'true';
-            document.body.classList.add('ring-mod-on');
-            ringModEl.classList.add('ring-mod-on-button');
-            modKnobEl.classList.remove('off');
-            
-        } else {
-            ringMod.wet.value = 0;
-            ringModEl.ariaChecked = 'false';
-            document.body.classList.remove('ring-mod-on'); 
-            ringModEl.classList.remove('ring-mod-on-button');   
-            modKnobEl.classList.add('off');
-        }
-    }
-}
+// FUNCTIONS
 
 const setStopTimer = () => {
-    timeLeft = (player.buffer.duration - 2 * (parseFloat(Tone.Transport.position.split(':')[0]) + parseFloat(Tone.Transport.position.split(':')[1])/4 + parseFloat(Tone.Transport.position.split(':')[2])/16)) * 1000;
-        
-    stopTimer = setTimeout(handleEnd, timeLeft)
-}
+  const timeLeft =
+    (player.buffer.duration -
+      2 *
+        (parseFloat(Tone.Transport.position.split(":")[0]) +
+          parseFloat(Tone.Transport.position.split(":")[1]) / 4 +
+          parseFloat(Tone.Transport.position.split(":")[2]) / 16)) *
+    1000;
+
+  stopTimer = setTimeout(handleEnd, timeLeft);
+};
 
 const clearStopTimer = () => {
-    clearTimeout(stopTimer);
-    stopTimer = null;
-}
+  clearTimeout(stopTimer);
+  stopTimer = null;
+};
+
+// HANDLERS
+
+const handleEngage = () => {
+  if (powerOn) {
+    power.classList.remove("on");
+    powerOn = false;
+
+    dist.disconnect();
+    Tone.Master.volume.value = -10;
+
+    document.body.classList.remove("ring-mod-on");
+    ringModEl.classList.remove("ring-mod-on-button");
+    modKnobEl.classList.add("off");
+    return;
+  }
+  power.classList.add("on");
+  powerOn = true;
+
+  Tone.Master.volume.value = storedVol;
+  player.chain(dist, filter, ringMod);
+
+  if (ringModEl.ariaChecked == "false") {
+    ringMod.wet.value = 0;
+  } else {
+    document.body.classList.add("ring-mod-on");
+    ringModEl.classList.add("ring-mod-on-button");
+    modKnobEl.classList.remove("off");
+  }
+};
+
+const handleRingMod = () => {
+  if (powerOn) {
+    if (ringModEl.ariaChecked == "false") {
+      ringMod.wet.value = 1;
+      ringModEl.ariaChecked = "true";
+      document.body.classList.add("ring-mod-on");
+      ringModEl.classList.add("ring-mod-on-button");
+      modKnobEl.classList.remove("off");
+    } else {
+      ringMod.wet.value = 0;
+      ringModEl.ariaChecked = "false";
+      document.body.classList.remove("ring-mod-on");
+      ringModEl.classList.remove("ring-mod-on-button");
+      modKnobEl.classList.add("off");
+    }
+  }
+};
 
 const handlePlay = () => {
-    Tone.start();
-    if (play.ariaChecked == 'false') {
-        play.ariaChecked = 'true';
-        play.innerHTML = '<i class="fa-solid fa-pause"></i>';
+  Tone.start();
+  if (play.ariaChecked == "false") {
+    play.ariaChecked = "true";
+    play.innerHTML = '<i class="fa-solid fa-pause"></i>';
 
-        setStopTimer();
-        Tone.Transport.start();
-    } else {
-        play.ariaChecked = 'false';
-        play.innerHTML = '<i class="fa-solid fa-play"></i>';
+    setStopTimer();
+    Tone.Transport.start();
+  } else {
+    play.ariaChecked = "false";
+    play.innerHTML = '<i class="fa-solid fa-play"></i>';
 
-        clearStopTimer();
-        Tone.Transport.pause();
-    }
-}
+    clearStopTimer();
+    Tone.Transport.pause();
+  }
+};
 
 const handleEnd = () => {
-    Tone.Transport.stop();
-    play.innerHTML = '<i class="fa-solid fa-play"></i>';
-    play.ariaChecked = 'false';
-}
+  Tone.Transport.stop();
+  play.innerHTML = '<i class="fa-solid fa-play"></i>';
+  play.ariaChecked = "false";
+};
 
 async function handleFileSelect(e) {
-    let file = e.target.files[0];
-    if (!file) { // user cancels dialog (file == undefined)
-        file = currentFile;
-        currentFile = null;
-    } else { // user chooses a file
-        clearStopTimer();
-        handleEnd();
-        player.unsync();
-        file = e.target.files[0];
-        currentFile = file;
+  let file = e.target.files[0];
+  if (!file) {
+    // user cancels dialog (file == undefined)
+    file = storedFile;
+    storedFile = null;
+  } else {
+    // user chooses a file
+    clearStopTimer();
+    handleEnd();
+    player.unsync();
+    file = e.target.files[0];
+    storedFile = file;
 
-        const arrayBuffer = await file.arrayBuffer();
-        const audioBuffer = await context.decodeAudioData(arrayBuffer);   
-        player = new Tone.Player(audioBuffer).toDestination();
-        player.sync().start(0);
-        if (powerOn) player.chain(dist, filter, ringMod); 
-    }
-    let filename_display = document.getElementById('file-upload');
-    // Update element's width to fit the filename
-    filename_display.setAttribute('style','height:auto;');
-    // Replace upload button label with name of uploaded file
-    filename_display.innerText = file.name;
+    const arrayBuffer = await file.arrayBuffer();
+    const audioBuffer = await context.decodeAudioData(arrayBuffer);
+    player = new Tone.Player(audioBuffer).toDestination();
+    player.sync().start(0);
+    if (powerOn) player.chain(dist, filter, ringMod);
+  }
+  let filename_display = document.getElementById("file-upload");
+  // Update element's width to fit the filename
+  filename_display.setAttribute("style", "height:auto;");
+  // Replace upload button label with name of uploaded file
+  filename_display.innerText = file.name;
 }
+
+// INIT
 
 const init = () => {
-    engage.addEventListener('click', handleEngage);
-    play.addEventListener('click', handlePlay);
-    ringModEl.addEventListener('click', handleRingMod);
-    fileEl.addEventListener('change', handleFileSelect);
+  engage.addEventListener("click", handleEngage);
+  play.addEventListener("click", handlePlay);
+  ringModEl.addEventListener("click", handleRingMod);
+  fileEl.addEventListener("change", handleFileSelect);
 
-    for (let knob of knobs) {   
-        knob.element.addEventListener('mousedown', knob.handleKnob);
-        knob.element.addEventListener('touchstart', knob.handleKnob);
-        knob.element.addEventListener('dblclick', knob.handleReset);
-        knob.element.addEventListener('touchend', knob.handleResetTouch);
-    }
-}
+  for (let knob of knobs) {
+    knob.element.addEventListener("mousedown", knob.handleKnob);
+    knob.element.addEventListener("touchstart", knob.handleKnob);
+    knob.element.addEventListener("dblclick", knob.handleReset);
+    knob.element.addEventListener("touchend", knob.handleResetTouch);
+  }
+};
 
 init();
